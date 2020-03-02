36b50f1ea0e7d94e56cebe245f9eca65074311a5
S. Makai Takori <makait87@gmail.com>
2020-03-02 23:54:30 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO users VALUES (
+
+
+
+)
+
+INSERT INTO users (name, age) VALUES
+("column 1 data", "column 2 data", "column 3 data"),
+("more data", "more data", "more data"),
+("more data", "more data", "more data");
